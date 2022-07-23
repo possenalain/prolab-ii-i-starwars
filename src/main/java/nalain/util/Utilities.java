583451b88a83e2b56317c3f74285826137792259
Nalain <possenalain21@gmail.com>
2022-07-23 22:40:27 +0300

    dynamically add signs on the board assign entrances

--- src/main/java/nalain/util/Utilities.java
+++ src/main/java/nalain/util/Utilities.java
@@ -2,13 +2,13 @@ package nalain.util;
 
 import nalain.PortableGameSetup;
 import nalain.gui.GameWindow;
-import nalain.karakterler.Karakter;
 import nalain.karakterler.bad.BadCharacter;
 import nalain.karakterler.bad.Darthvader;
 import nalain.karakterler.bad.Kyloren;
 import nalain.karakterler.bad.Stormtrooper;
 import nalain.karakterler.good.LukeSkyWalker;
 import nalain.karakterler.good.MasterYoda;
+import nalain.maze.LabyrinthSign;
 import nalain.maze.Location;
 
 import java.io.BufferedReader;
@@ -22,19 +22,21 @@ public class Utilities {
     public static PortableGameSetup portableGameSetup = PortableGameSetup.getInstance();
     static ArrayList<String> characters = new ArrayList<String>();
     static ArrayList<String> maze = new ArrayList<String>();
+    static ArrayList<String> indications = new ArrayList<String>();
     public static void initializeFromFile() throws IOException {
         BufferedReader filein = null;
         try {
             filein = new BufferedReader(new FileReader(portableGameSetup.getBASE_PATH_RESOURCES() + "Harita.txt"));
             String row;
             while ((row = (String) filein.readLine()) != null) {
-
                 if (row.startsWith("Karakter")) {
                     characters.add(row);
+                } else if (row.startsWith("Signs")) {
+                    indications.add(row);
                 } else if (row.startsWith("0") || row.startsWith("1")) {
                     maze.add(row);
                 } else {
-                    System.out.println("the row doesn't follow requested pattern");
+                    //System.out.println("the row doesn't follow requested pattern");
                 }
             }
         } catch (IOException e) {
@@ -56,23 +58,20 @@ public class Utilities {
                 portableGameSetup.getLabyrinth().mazeArrayWithLocations[i][j] = loc;
             }
         }
+        //initialize the signs too
+        initializeLabyrinthDoorsAsSigns();
     }
-
     public static void initializeGoodCharacter() {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("[skywalker:1] [Master yoda:2]:");
-
         int choice = scan.nextInt();
-        ;
-
         if (choice == 1)
             portableGameSetup.setIyikarakter(new LukeSkyWalker());
         else if (choice == 2)
             portableGameSetup.setIyikarakter(new MasterYoda());
 
     }
-
     public static void initializeBadCharacters() {
 
         for (int i = 0; i < characters.size(); i++) {
@@ -81,6 +80,7 @@ public class Utilities {
             String splittedrow[] = row.split("\\W");
 
             BadCharacter karacter = null;
+
             if (splittedrow[1].equalsIgnoreCase("darthvader"))
                 karacter = new Darthvader();
 
@@ -91,14 +91,33 @@ public class Utilities {
                 karacter = new Kyloren();
 
             if (karacter != null) {
-                karacter.setKapi(splittedrow[3]);
-                karacter.setInitialLocation(portableGameSetup.getLabyrinth().getDoor(splittedrow[3]));
+                String entrance = splittedrow[3];
+                karacter.setEntrance(entrance);
+                karacter.setInitialLocation(portableGameSetup.getLabyrinth().findSignByName(entrance));
                 karacter.setCurrentlocation(karacter.getInitialLocation());
                 portableGameSetup.getKotukarakter().add(karacter);
             }
         }
     }
 
+    public static void initializeLabyrinthDoorsAsSigns() {
+
+        for (String row : indications) {
+
+            String splittedrow[] = row.split("\\W");
+
+            String name = splittedrow[1];
+            int x = Integer.parseInt(splittedrow[3]);
+            int y = Integer.parseInt(splittedrow[4]);
+            String iconName = splittedrow[6];
+
+            LabyrinthSign labyrinthSign = new LabyrinthSign();
+            labyrinthSign.setName(name);
+            labyrinthSign.setIconName(iconName);
+            labyrinthSign.setCoordinates(new Location(x, y));
+            portableGameSetup.getLabyrinth().getLabyrinthSigns().add(labyrinthSign);
+        }
+    }
     public static void InitializeGameWindow() {
         GameWindow gameWindow = new GameWindow();
         portableGameSetup.setGameWindow(gameWindow);
